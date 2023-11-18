fa749a7eeb08beead8acc8afa5e2460b0fa305d2
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-18 14:38:43 -0500

    Flush out basic functionality.

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,22 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboard_index_url
+  include Devise::Test::IntegrationHelpers
+
+  test "redirects unauthorized users" do
+    get dashboard_url
+    assert_redirected_to new_user_session_path
+  end
+
+  test "permits authorized users" do
+    sign_in users(:one)
+    get dashboard_url
     assert_response :success
+  end
+
+  test "renders username" do
+    sign_in users(:one)
+    get dashboard_url
+    assert_match "jsmith", response.body
   end
 end
