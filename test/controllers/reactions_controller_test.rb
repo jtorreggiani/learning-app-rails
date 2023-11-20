1a1d909d3310961e407ad139fa0a78d938291be5
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-19 23:14:05 -0500

    Make rubocop happy.

--- test/controllers/reactions_controller_test.rb
+++ test/controllers/reactions_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReactionsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
@@ -7,16 +9,16 @@ class ReactionsControllerTest < ActionDispatch::IntegrationTest
     sign_in @user # If you're using Devise for authentication
   end
 
-  test "should create reaction with turbo stream" do
+  test 'should create reaction with turbo stream' do
     assert_difference('Reaction.count') do
       post post_reactions_path(@post), params: { reaction_type: 'thumbs_up' }, as: :turbo_stream
     end
 
     assert_response :success
-    assert_match /turbo-stream/, @response.body
+    assert_match(/turbo-stream/, @response.body)
   end
 
-  test "should handle failed reaction creation with turbo stream" do
+  test 'should handle failed reaction creation with turbo stream' do
     # Assuming there's some validation in Reaction model that can cause failure
     # For example, let's say a reaction can't be 'dislike' for this test
     assert_no_difference('Reaction.count') do
@@ -24,7 +26,7 @@ class ReactionsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response :success
-    assert_match /turbo-stream/, @response.body
+    assert_match(/turbo-stream/, @response.body)
     # You can also assert specific contents in the turbo stream response if needed
   end
 end
