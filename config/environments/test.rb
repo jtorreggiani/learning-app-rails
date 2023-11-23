46d439ef1ea07487b660deaf1c76199d245bccda
Joe Torreggiani <joetorreggiani@gmail.com>
2023-11-23 01:13:20 -0500

    Fix issue in CI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV['CI'].present?
 
+  config.assets.css_compressor = nil
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
