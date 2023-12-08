78f17f53e8b7c01767d54f0c7ac88851cc523177
Joe Torreggiani <joetorreggiani@gmail.com>
2023-12-08 00:01:10 -0500

    Add tests for posts.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
-# == Schema Information
-#
-# Table name: posts
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  title      :string
-#  content    :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint
-#
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save post without title' do
+    post = Post.new
+    assert_not post.save, 'Saved the post without a title'
+  end
+
+  test 'should save post with title' do
+    post = Post.new(title: 'Title')
+    assert post.save, 'Saved the post with a title'
+  end
+
+  test 'should not save post without user' do
+    post = Post.new(title: 'Title')
+    assert post.save, 'Saved the post with a title'
+  end
+
+  test 'should save post with user' do
+    user = users(:one)
+    post = Post.new(title: 'Title', user: user)
+    assert post.save, 'Saved the post with a title'
+  end
+
+  test 'should destroy reactions when post is destroyed' do
+    post = posts(:one)
+    assert_difference 'Reaction.count', -1 do
+      post.destroy
+    end
+  end
+
+  test 'should not destroy user when post is destroyed' do
+    post = posts(:one)
+    assert_no_difference 'User.count' do
+      post.destroy
+    end
+  end
 end
