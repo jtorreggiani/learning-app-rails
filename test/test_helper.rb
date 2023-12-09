02e807ae7a69850d3ee2ca4ae6db39ed1ea860ca
Joe Torreggiani <joetorreggiani@gmail.com>
2023-12-08 20:34:49 -0500

    Make rubocop happy.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,7 @@ require 'rails/test_help'
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    if ENV['CI']
-      parallelize(workers: :number_of_processors)
-    end
+    parallelize(workers: :number_of_processors) if ENV['CI']
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
     # Add more helper methods to be used by all tests here...
