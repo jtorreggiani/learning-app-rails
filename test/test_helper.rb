b877d5e7830f51666fbc991a3c701fd124db6869
Joe Torreggiani <joetorreggiani@gmail.com>
2023-12-08 20:32:44 -0500

    Commit change to test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,9 @@ require 'rails/test_help'
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    if ENV['CI']
+      parallelize(workers: :number_of_processors)
+    end
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
     # Add more helper methods to be used by all tests here...
